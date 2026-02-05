41c8cbcfb2a40abcd3febefc3fdf14fb7dcc7581
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-02-05 14:09:23 +0100

    feat(scheduler): activation et configuration du moteur APScheduler

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_apscheduler import APScheduler
 
 db = SQLAlchemy()
+schedule = APScheduler()
 
 
 def creer_application():
@@ -13,6 +15,13 @@ def creer_application():
     # Initialise la DB avec l'instance de l'app
     db.init_app(app)
     from app.routes import bp
+
+    # schedule config
+    # Charge la configuration (liste JOBS)
+    schedule.init_app(app)
+    # Lancement d'un thread parallèle en arrière-plan 
+    # pour surveiller l'heure sans bloquer le site web.
+    schedule.start()
 
     # injection des routes de 'bp' dans l'instance 'app'
     app.register_blueprint(bp)
