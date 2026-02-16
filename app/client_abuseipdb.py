e36cb6e1748c0f41f260a92c7f57b8d6753d9c2d
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-02-16 16:56:19 +0100

    feat: implémentation du client AbuseIPDB avec logique de cache permanent

--- app/client_abuseipdb.py
+++ app/client_abuseipdb.py
@@ -1,27 +1,58 @@
 import requests
 import os
 from dotenv import load_dotenv
+from app.models import CacheIP
+from app import db
+
 
 load_dotenv()
 def verification_ip(ip):
+
+
     url = 'https://api.abuseipdb.com/api/v2/check'
 
-    clef_api =os.environ.get("api_key")
+    cache = CacheIP.query.get(ip)
+    if cache:
+        return cache.score, cache.code_pays
+    else:
 
-    querystring = {
-        'ipAddress': ip,
-        'maxAgeInDays': '90'
-    }
+        clef_api =os.environ.get("api_key")
 
-    headers = {
-        'Accept': 'application/json',
-        'Key': clef_api
-    }
+        # Paramètres de la requête GET
+        querystring = {
+            'ipAddress': ip,
+            'maxAgeInDays': '90'
+        }
 
-    reponse = requests.get(url,headers=headers, params=querystring)
+        headers = {
+            'Accept': 'application/json',
+            'Key': clef_api
+        }
+        
+        try:
+            reponse = requests.get(url, params=querystring, headers=headers)
 
-    if reponse.status_code==200:
+            if reponse.status_code == 200:
+                json_response = reponse.json()
+                data = json_response['data']
+                score = data.get('abuseConfidenceScore')
+                pays = data.get('countryCode')
 
-        data = reponse.json()
-        return data['data']['abuseConfidenceScore']
+                try:
+                    # SAUVEGARDE
+                    nouveau = CacheIP(ip=ip, score=score, code_pays=pays)
+                    db.session.add(nouveau)
+                    db.session.commit()
+                except Exception as e:
+                    db.session.rollback()
+                    print(f"erreur: {e}")
+                
+                return score, pays
+            else:
+                print(f"Erreur: {reponse.status_code}")
+                return None, None
+            
 
+        except Exception as e:
+            print(f"erreur:{e}")
+            return None, None
