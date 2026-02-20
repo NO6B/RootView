7f6822b46f764dd0ceafd00e2201fd2cb7f223c3
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-02-20 16:15:17 +0100

    feat(app): intégration du LoginManager et configuration du chargement utilisateur

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_apscheduler import APScheduler
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 schedule = APScheduler()
-
+login_manager = LoginManager()
 
 def creer_application():
     # Instanciation du conteneur principal de l'application Flask
@@ -23,7 +24,18 @@ def creer_application():
     # pour surveiller l'heure sans bloquer le site web.
     schedule.start()
 
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'main.login'
+
     # injection des routes de 'bp' dans l'instance 'app'
     app.register_blueprint(bp)
+
+    from app.models import Utilisateur
+   # ajout de la fonction load_user a l'instance de login_manager 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Utilisateur.query.get(int(user_id))
+
 
     return app
