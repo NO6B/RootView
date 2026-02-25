1b88fcafdb0572d92410963c1cabe22ea2d1a769
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-02-25 21:05:12 +0100

    refactor(parser): nettoyage des variables inutilisées et amélioration de la regex web

--- app/services/parser.py
+++ app/services/parser.py
@@ -4,30 +4,29 @@ def parser_ligne_log(ligne_brute):
     if not ligne_brute or not ligne_brute.strip():
         return None
 
-    #  SSH: Linux System Logs
+    #  SSH
     regex_ssh = r"^(.+?)\s+.*?sshd\[\d+\]:\s+(.*?)(?:\s+from)?\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
     match_ssh = re.search(regex_ssh, ligne_brute)
 
     if match_ssh:
         return {
-            "source": "SSH",
             "date": match_ssh.group(1).strip(),
             "message": match_ssh.group(2).strip(),
             "adresse_ip": match_ssh.group(3),
-            "ligne_complete": ligne_brute,
         }
 
-    # WEB: Apache
-    regex_web = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s-\s-\s\[(.*?)\]\s\"(.*?)\""
+    # WEB
+    regex_web = r"^(\d{1,3}(?:\.\d{1,3}){3})\s-\s-\s\[(.*?)\]\s\"((\S+)\s(.*?)\s.*?)\"\s(\d{3})"
     match_web = re.search(regex_web, ligne_brute)
 
     if match_web:
         return {
-            "source": "WEB",
+            "adresse_ip": match_web.group(1),
             "date": match_web.group(2),
             "message": match_web.group(3),
-            "adresse_ip": match_web.group(1),
-            "ligne_complete": ligne_brute,
+            "methode": match_web.group(4),
+            "url": match_web.group(5),
+            "status_code": match_web.group(6),
         }
 
     return None
